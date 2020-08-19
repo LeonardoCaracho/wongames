d786306b3071c7b51aa2d35829cd3c3cc37c55ce
Leonardo <leeo.apc@gmail.com>
2020-08-19 10:21:41 -0300

    create and add plugins

---
+++ wongames-api/plugins/repositories/admin/src/containers/HomePage/index.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, memo } from 'react';
+import axios from 'axios'
+import styled from 'styled-components';
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`
+
+const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/React-avancado/repos")
+      .then(res => setRows(res.data))
+      .catch(e => strapi.notification.error(`Ops... github API error. ${e}`));
+  }, [])
+
+  const headers = [
+    {
+      name: 'Name',
+      value: 'name',
+    },
+    {
+      name: 'Description',
+      value: 'description',
+    },
+    {
+      name: 'Url',
+      value: 'url',
+    },
+  ];
+
+  return (
+    <Wrapper>
+      <Header
+        title={{ label: 'React Avançado Repositories' }}
+        content="A list of our repositories."
+      />
+      <Table headers={headers} rows={rows}/>
+    </Wrapper>
+  );
+};
+
+export default memo(HomePage);
