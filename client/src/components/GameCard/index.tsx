8c15b229f810a9e1704a57a88633c6ccefd21a9d
Leonardo <leeo.apc@gmail.com>
2020-11-04 08:54:12 -0300

    create gamecard

---
+++ client/src/components/GameCard/index.tsx
@@ -0,0 +1,37 @@
+import {
+  FavoriteBorder,
+  AddShoppingCart
+} from '@styled-icons/material-outlined'
+import * as S from './styles'
+
+import Button from '../Button'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+}
+
+const GameCard = ({ title, developer, img, price }: GameCardProps) => (
+  <S.Wrapper>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button">
+        <FavoriteBorder aria-label="Add to wishlist" />
+      </S.FavButton>
+      <S.BuyBox>
+        <S.Price>{price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default GameCard
