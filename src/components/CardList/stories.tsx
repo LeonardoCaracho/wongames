2743b3bca5af81ba0d995c7e9ba4eff14b377694
Leonardo Caracho <leeo.apc@gmail.com>
2020-12-21 09:53:51 -0300

    create CartList Component

---
+++ src/components/CardList/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import CardList, { CardListProps } from '.'
+
+import mockItems from './mock'
+
+export default {
+  title: 'CardList',
+  component: CardList,
+  args: {
+    total: '$ 60,00',
+    items: mockItems
+  },
+  argTypes: {
+    items: {
+      type: ''
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CardListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CardList {...args} />
+  </div>
+)
---
+++ src/components/CardList/test.tsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import CardList from '.'
+import mockItems from './mock'
+
+const props = {
+  items: mockItems,
+  total: '$ 60,00'
+}
+
+describe('<CardList />', () => {
+  it('should render the cart list', () => {
+    const { container } = renderWithTheme(<CardList {...props} />)
+
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+    expect(screen.getByText(`${props.total}`)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
