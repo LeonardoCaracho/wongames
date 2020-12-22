1540d887db5e5fd86a993995177692f368392e2b
Leonardo Caracho <leeo.apc@gmail.com>
2020-12-22 19:14:46 -0300

    create cards list

---
+++ src/components/CardsList/index.tsx
@@ -0,0 +1,24 @@
+import { PaymentCard } from 'components/PaymentOptions'
+import Heading from '../Heading'
+
+import * as S from './styles'
+
+export type CardsListProps = {
+  cards?: PaymentCard[]
+}
+
+const CardsList = ({ cards }: CardsListProps) => (
+  <>
+    <Heading lineBottom color="black" size="small">
+      My cards
+    </Heading>
+    {cards?.map((card) => (
+      <S.Card key={card.number}>
+        <img src={card.img} alt={card.flag} />
+        <span>{card.number}</span>
+      </S.Card>
+    ))}
+  </>
+)
+
+export default CardsList
