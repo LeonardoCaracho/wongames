35dadf5b23afb9c865de5375737358b5d4912c9c
Leonardo Caracho <leeo.apc@gmail.com>
2020-12-23 10:53:28 -0300

    create profile page

---
+++ src/components/OrdersList/index.tsx
@@ -0,0 +1,31 @@
+import GameItem, { GameItemProps } from '../GameItem'
+import Empty from '../Empty'
+import Heading from '../Heading'
+
+import * as S from './styles'
+
+export type OrdersListProps = {
+  items?: GameItemProps[]
+}
+
+const OrdersList = ({ items }: OrdersListProps) => (
+  <S.Wrapper>
+    <Heading lineBottom lineColor="primary" color="black" size="small">
+      My orders
+    </Heading>
+
+    {items?.length ? (
+      items?.map((item) => (
+        <GameItem key={item.downloadLink} {...item}></GameItem>
+      ))
+    ) : (
+      <Empty
+        title="You have no orders yet"
+        description="Go back to the store and explore great games and offers"
+        hasLink
+      ></Empty>
+    )}
+  </S.Wrapper>
+)
+
+export default OrdersList
